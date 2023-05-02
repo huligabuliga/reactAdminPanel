93be114539cb6eb4ad6e8e781b84cc44e4547cb0
Jonas Clausen <jonastclausen@gmail.com>
2023-05-02 14:25:10 -0600

    Update Widget.jsx

--- reactadminpanel/src/components/widgets/Widget.jsx
+++ reactadminpanel/src/components/widgets/Widget.jsx
@@ -1,20 +1,54 @@
 import "./widget.scss"
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import AccountBalanceWalletOutlinedIcon from '@mui/icons-material/AccountBalanceWalletOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+
 const Widget = ({type}) => {
 
     let data; 
 
+    //temp 
+    const amount = 5000;
+    const diff = 20;
     switch(type){
         case "user":
             data = {
                 title: "USERS",
                 isMoney: false,
                 link: "See all users",
-                icon: (<PersonOutlineOutlinedIcon className="icon"/>),
+                icon: (<PersonOutlineOutlinedIcon className="icon" style={{color:"crimson", backgroundColor: "rgba(255,0,0,0.2)"}} />),
 
             }
             break;
+            case "order":
+                data = {
+                    title: "ORDERS",
+                    isMoney: false,
+                    link: "View all orders",
+                    icon: (<ShoppingCartOutlinedIcon className="icon" style={{color:"goldenrod", backgroundColor: "rgba(218,165,32,0.2)"}}/>),
+    
+                }
+                break;
+                case "earnings":
+                    data = {
+                        title: "EARNINGS",
+                        isMoney: true,
+                        link: "View net earnings",
+                        icon: (<MonetizationOnOutlinedIcon className="icon" style={{color:"green", backgroundColor: "rgba(0,128,0,0.2)"}}/>),
+        
+                    }
+                    break;
+                    case "balance":
+                        data = {
+                            title: "BALANCE",
+                            isMoney: true,
+                            link: "See details",
+                            icon: (<AccountBalanceWalletOutlinedIcon className="icon" style={{color:"purple", backgroundColor: "rgba(128,0,128,0.2)"}}/>),
+            
+                        }
+                        break;
             default:
                 break;
     
@@ -25,16 +59,16 @@ const Widget = ({type}) => {
   return (
     <div className="widget">
         <div className="left">
-            <span className="title">USERS</span>
-            <span className="counter">21312</span>
-            <span className="link">See all users</span>
+            <span className="title">{data.title}</span>
+            <span className="counter">{data.isMoney && "$"} {amount}</span>
+            <span className="link">{data.link}</span>
         </div>
         <div className="right">
             <div className="percentage positive">
                 <KeyboardArrowUpIcon/>
-                20%
+                {diff} %
                 </div>
-        <PersonOutlineOutlinedIcon className="icon"/>
+        {data.icon}
         </div>
     </div>
   )
